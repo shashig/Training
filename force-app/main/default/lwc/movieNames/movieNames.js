41ab72495bccc964d66c15bc73352de9511e65a4
Shashidhar Gurumurthy <shashidhar.gurumurthy@gmail.com>
2026-02-04 16:44:17 +0530

    Debugging recordId issues

--- force-app/main/default/lwc/movieNames/movieNames.js
+++ force-app/main/default/lwc/movieNames/movieNames.js
@@ -1,13 +1,27 @@
 import { LightningElement, wire, api } from 'lwc';
 import getMovieArtists from '@salesforce/apex/MovieController.getMovieArtists';
-
+ 
 export default class MovieNames extends LightningElement {
-  @api recordId = '';
-
+  @api recordId;
+  
   movieArtistsResponse;
   @wire(getMovieArtists, { movieId: "$recordId"}) 
   wiredMovies(response) {
+    console.log('recordId: ' + this.recordId);
     console.log(response);
     this.movieArtistsResponse = response;
+  }
+
+  get displayRecs() {
+    if (this.movieArtistsResponse.data) {
+      return this.movieArtistsResponse.data.map(ma => {
+        if (ma.RecordType.Name === 'Actor') {
+          return `Actor: ${ma.Artist__r.Name} - ${ma.Character__c}`;
+        } else {
+          return `Crew: ${ma.Artist__r.Name} - ${ma.Job__c}`
+        }
+      })
+    }
+    return [];
   }
 }
---
+++ force-app/main/default/lwc/simpleRecordIdCheck/simpleRecordIdCheck.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class SimpleRecordIdCheck extends LightningElement {
+  @api recordId;
+}
